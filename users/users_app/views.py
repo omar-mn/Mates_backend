40e5ffd276f1e5eeec90fa3d879ae42e7a241070
omar <omar@gmail.com>
2026-07-10 01:05:06 +0300

    دا ايه دا والله

--- users/users_app/views.py
+++ users/users_app/views.py
@@ -23,4 +23,4 @@ def GetProfile(request , pk):
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def healthcheck(request):
-    return (Response({"لا داعي للسيطرة الامور تحت القلق  /  بتيست الوورك فلوو / للمرة التالتة"}))
+    return (Response({"لا داعي للسيطرة الامور تحت القلق  /  بتيست الوورك فلوو / للمرة ال ؟؟؟"}))
